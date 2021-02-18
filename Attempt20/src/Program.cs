8929a719a89be9f30d29ff7deb7d5a82b0968605
Josh Kearney <joshwkearney@outlook.com>
2021-02-18 16:46:24 -0600

    Reorganize project

---
+++ Attempt20/src/Program.cs
@@ -0,0 +1,21 @@
+﻿using Attempt20.Analysis;
+using System;
+using System.IO;
+
+namespace Attempt20 {
+    public class Program {
+        public static void Main(string[] args) {
+            try {
+                var c = new TrophyCompiler(File.ReadAllText("Resources/Program.txt")).Compile();
+
+                Console.WriteLine(c);
+                File.WriteAllText("Resources/Output.txt", c);
+            }
+            catch (TrophyException ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
